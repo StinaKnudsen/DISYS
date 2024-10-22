0ded65b076f9095c78b5cb826fb6d768f87f8490
AVNBuelow <alby@itu.dk>
2024-10-22 10:04:33 +0200

    mod tidy

---
+++ client/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	proto "ChittyServer/grpc"
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := proto.NewChittyChatServiceClient(conn)
+
+	joinResp, err := client.Join(context.Background(), &proto.JoinRequest{ParticipantId: "client1"})
+	if err != nil {
+		log.Fatalf("Join failed: %v", err)
+	}
+	log.Printf("Join response: %s", joinResp.WelcomeMessage)
+}
---
+++ server/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	proto "ChittyServer/grpc"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	proto.UnimplementedChittyChatServiceServer
+	// Unfinished
+}
+
+// Make the grpc methods
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	proto.RegisterChittyChatServiceServer(grpcServer, &server{})
+
+	log.Printf("Server listening at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
